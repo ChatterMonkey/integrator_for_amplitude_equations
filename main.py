014ea9e9ffa4a256fba606ed2a62c28cb1fc1241
ChatterMonkey <basu.maya@icloud.com>
2024-02-10 18:33:06 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,94 @@
+from typing import List, Union
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+lamb = 2
+eps = 0.01
+dt = 0.005
+N = 10000
+trajdata = []
+
+#each row is re(A1), im(A1), re(A2), im(A2)
+initial_components = np.array([[1, 2, 3, 4], [4, 3, 2,6], [0, 0, 0,0]])
+
+print(initial_components.shape[0])
+
+
+for run in range(0,initial_components.shape[0]):
+    A1  = initial_components[run,0] + initial_components[run,1]*1j
+    A2 = initial_components[run, 2] + initial_components[run, 3] * 1j
+    trajdata.append([A1, A2, lamb, eps, dt, N])
+
+print(trajdata)
+
+def label(A1, A2, lamb, eps, dt, N):
+    return ('traj_(' + str(A1) + ',' + str(A2) + ')_' + str(lamb) + '_' + str(eps) + '_' + str(dt) + '_' + str(N) + '.npy')
+
+def f(A, lamb, eps):
+    A1, A2 = A
+
+    array = np.zeros(2, dtype=np.complex64)
+    s = np.ones((2, 2))
+    array[0] = 1j * lamb * A1 + eps * A2 + 1j * A1 * (s[0, 0] * np.abs(A1) ** 2 + s[0, 1] * np.abs(A2) ** 2)
+    array[1] = eps * A1 + 1j * A2 * (s[1, 0] * np.abs(A1) ** 2 + s[1, 1] * np.abs(A2) ** 2)
+    return array
+
+
+def compute_traj(trajdata):  # calculates and stores trajectories, given a list of their data, returns dictionary of trajectories
+    trajs = dict()
+    for [A1, A2, lamb, eps, dt, N] in trajdata:
+
+        traj = np.zeros((N, 2), dtype=np.complex64)
+        traj[0, :] = A1, A2
+        for i in range(0, N - 1):
+            a1 = dt * f(traj[i, :], lamb, eps)
+            a2 = dt * f(traj[i, :] + a1 / 2, lamb, eps)
+            a3 = dt * f(traj[i, :] + a2 / 2, lamb, eps)
+            a4 = dt * f(traj[i, :] + a3, lamb, eps)
+            traj[i + 1, :] = traj[i, :] + (a1 + 2 * a2 + 2 * a3 + a4) / 6
+
+        name = label(A1, A2, lamb, eps, dt, N)
+        trajs[name] = traj
+        np.save('trajectories/' + name, traj)
+        print("Computed " + name + ":")
+        print(traj[0:3])
+        print("...")
+        print(traj[N - 3:N])
+    return trajs
+
+def show_phasespace(N, dt, trajs):
+    ax = plt.figure().add_subplot(projection='3d')
+    values = list(trajs.values())
+    for trajectory in values:
+
+        ax.plot(np.imag(trajectory[:, 0]), np.real(trajectory[:, 0]), np.real(trajectory[:, 1]))
+    plt.show()
+def show_components(N, dt, trajs):
+    ax = plt.figure().add_subplot()
+    time = np.arange(N) * dt
+    values = list(trajs.values())
+    for trajectory in values:
+        ax.plot(time, np.real(trajectory[:, 0]))
+
+    plt.show()
+
+def reconstitute(trajdata):
+    trajs = {}
+    for data in trajdata:
+        name = label(*data)
+        trajs[name] = np.load("trajectories/" + name)
+
+    return trajs
+
+
+
+trajs = compute_traj(trajdata)
+#trajs = reconstitute(trajdata)
+
+show_phasespace(N,dt, trajs)
+#show_components(N,dt, trajs)
+
+
+
+# RK4
